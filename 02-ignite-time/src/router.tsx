62c2b651d17caae4262892f69f997ee5cc6f1933
MatheusMelloDev <165337919+MatheusMelloDev@users.noreply.github.com>
2024-09-26 15:31:42 -0300

    Reorganizando o código e criando a home e a página histórico

--- 02-ignite-time/src/router.tsx
+++ 02-ignite-time/src/router.tsx
@@ -1,6 +1,6 @@
 import {Routes , Route} from 'react-router-dom'
- import Home  from './pages/home';
-import History from './pages/history';
+ import {Home} from './pages/home/index';
+import {History} from './pages/history/index';
 import { DefaultLayout } from './layouts/defaultLayout';
 
 export function Router () {
